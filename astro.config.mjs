ea39b34ffb99420b8df0932c1933f14c537d6f50
raiyanhaider <150611836+raiyanhaider@users.noreply.github.com>
2025-07-03 04:32:57 +0600

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,26 +6,8 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind({
-      applyBaseStyles: false, // Reduce CSS bundle size
-    }),
+    tailwind(),
     react()
   ],
-  compressHTML: true,
-  build: {
-    inlineStylesheets: 'auto',
-    assets: 'assets'
-  },
-  vite: {
-    build: {
-      cssCodeSplit: true,
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            'glide': ['@glidejs/glide']
-          }
-        }
-      }
-    }
-  }
+  compressHTML: true
 });
